f5e727b2e75aeb138211391a23f8528aba5e52b1
nhuanh9 <chopperlouis@gmail.com>
2019-08-04 14:03:55 +0700

    them 3 phan tu dau

--- src/main/java/com/codegym/repository/impl/ReceptionistRepositoryImpl.java
+++ src/main/java/com/codegym/repository/impl/ReceptionistRepositoryImpl.java
@@ -3,13 +3,21 @@ package com.codegym.repository.impl;
 import com.codegym.model.Receptionist;
 import com.codegym.repository.ReceptionistRepository;
 
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ReceptionistRepositoryImpl implements ReceptionistRepository {
+    private static final Charset UTF_8 = Charset.forName("UTF-8");
+    private static final Charset ISO = Charset.forName("ISO-8859-1");
 
-    private List<Receptionist> receptionistList = new ArrayList<>();
-
+    private static List<Receptionist> receptionistList ;
+    static {
+        receptionistList = new ArrayList<>();
+        receptionistList.add(new Receptionist(1,"Quinn", 10,"Việt Nam","Bay","1.png"));
+        receptionistList.add(new Receptionist(2,"Tris", 20,"Yodle","Bắn","2.jpg"));
+        receptionistList.add(new Receptionist(3,"Veigar", 30,"Pintorver","Farm","3.jpg"));
+    }
     @Override
     public List<Receptionist> findAll() {
         return receptionistList;
